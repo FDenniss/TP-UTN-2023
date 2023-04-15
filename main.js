c953f77fac9c32dee9319cbbdd57e122a3248af2
FDenniss <93162753+FDenniss@users.noreply.github.com>
2023-04-14 21:10:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("form");
 const inputName = document.getElementById("input-name");
 const errorMsg = document.getElementById("error-msg");
-
+const button = location.href("index0.html")
 form.onsubmit = (e) => {
   e.preventDefault();
   errorMsg.style.display = "none";
@@ -12,3 +12,8 @@ form.onsubmit = (e) => {
     sessionStorage.setItem("Name", inputName.value);
   }
 };
+
+function submit() {
+  window.location.href="index0.html";
+}
+
